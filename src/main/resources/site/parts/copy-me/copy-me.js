5ac53a515069e8adc18aecfc20a1b0490bb160f7
Bellfalasch <bellfalasch@gmail.com>
2016-06-08 20:39:07 +0200

    Adding "copy-me" files

---
+++ src/main/resources/site/parts/copy-me/copy-me.js
@@ -0,0 +1,31 @@
+var libs = {
+	portal: require('/lib/xp/portal'),
+	content: require('/lib/xp/content'),
+	thymeleaf: require('/lib/xp/thymeleaf')
+};
+
+var conf = {
+	view: resolve('copy-me.html') // TODO: This is not the view-file you're looking for ... or is it?
+};
+
+exports.get = function(req) {
+
+	/* ### Collect ### */
+	var content = libs.portal.getContent();
+	var component = libs.portal.getComponent();
+
+	/* ### Manipulate ### */
+	// TODO: if needed, manipulate the collected data here
+
+	/* ### Prepare ### */
+	var params = {
+		content: content,
+		component: component
+	}
+
+	/* ### Return ### */
+	return {
+		body: libs.thymeleaf.render(conf.view, params)
+	};
+
+};
