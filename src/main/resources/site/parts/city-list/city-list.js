e935cbf8334de91bfd2770177d5a3c4f88fadbfb
Bobby <bellfalasch@gmail.com>
2016-11-30 15:30:35 +0100

    Remove Google maps stuff

--- src/main/resources/site/parts/city-list/city-list.js
+++ src/main/resources/site/parts/city-list/city-list.js
@@ -12,14 +12,6 @@ var conf = {
 // Handle the GET request
 exports.get = function (req) {
 
-    // Get the part configuration for the map
-    var config = libs.portal.getComponent().config;
-    var zoom = parseInt(config.zoom) && config.zoom <= 15 && config.zoom >= 1 ? config.zoom : 10;
-    var mapType = config.mapType || 'ROADMAP';
-
-    // String that will be inserted to the head of the document
-    var googleMaps = '<script src="http://maps.googleapis.com/maps/api/js"></script>';
-
     var countryPath = libs.portal.getContent()._path;
 
     // Get all the country's cities
@@ -37,8 +29,6 @@ exports.get = function (req) {
     var cities = [];
 
     if (hits.length > 0) {
-        googleMaps += '<script>function initialize() {';
-
         // Loop through the contents and extract the needed data
         for (var i = 0; i < hits.length; i++) {
 
@@ -48,21 +38,7 @@ exports.get = function (req) {
             city.population = hits[i].data.population ? 'Population: ' + hits[i].data.population : null;
 
             cities.push(city);
-
-            if (city.location) {
-                city.mapId = 'googleMap' + i;
-
-                googleMaps += 'var center' + i + ' = new google.maps.LatLng(' + city.location + '); ';
-
-                googleMaps += 'var mapProp = {center:center' + i + ', zoom:' + zoom +
-                              ', mapTypeId:google.maps.MapTypeId.' + mapType + ', scrollwheel: false };' +
-                              'var map' + i + ' = new google.maps.Map(document.getElementById("googleMap' + i + '"),mapProp); ' +
-                              'var marker = new google.maps.Marker({ position:center' + i + '}); marker.setMap(map' + i + ');';
-            }
-
         }
-
-        googleMaps += '} google.maps.event.addDomListener(window, "load", initialize);</script>';
     }
 
     // Prepare the model object that will be passed to the view file
@@ -72,10 +48,6 @@ exports.get = function (req) {
 
     // Return the response object
     return {
-        body: libs.thymeleaf.render(conf.view, model),
-        // Put the maps' javascript into the head of the document
-        pageContributions: {
-            headEnd: googleMaps
-        }
+        body: libs.thymeleaf.render(conf.view, model)
     }
 };
