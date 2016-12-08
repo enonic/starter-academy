e570b065d3f735f10d118420ef77b69fdf5717f0
Bobby <bellfalasch@gmail.com>
2016-12-08 11:50:39 +0100

    Null check for image

--- src/main/resources/site/parts/banner/banner.js
+++ src/main/resources/site/parts/banner/banner.js
@@ -14,16 +14,19 @@ exports.get = function(req) {
 	var config = component.config;
 
 	/* ### Manipulate ### */
-	// portal.imageUrl - http://repo.enonic.com/public/com/enonic/xp/docs/6.8.0/docs-6.8.0-libdoc.zip!/module-lib_xp_portal.html#.imageUrl
-	config.imageUrl = libs.portal.imageUrl({
-		id: config.imageKey,
-		scale: 'block(1024,150)'
-	});
+	// Make sure you check for actual values (not null) before running any portal function (as they crash on null).
+	if (config.imageKey) {
+		// portal.imageUrl - http://repo.enonic.com/public/com/enonic/xp/docs/6.8.0/docs-6.8.0-libdoc.zip!/module-lib_xp_portal.html#.imageUrl
+		config.imageUrl = libs.portal.imageUrl({
+			id: config.imageKey,
+			scale: 'block(1024,150)'
+		});
+	}
 
 	/* ### Prepare ### */
 	var model = {
 		config: config
-	}
+	};
 
 	/* ### Return ### */
 	return {
