fcf71fd4a89703d5bdc0d2ac787adcdfa9db533e
Persijn Kwekkeboom <pkw@enonic.com>
2021-10-04 12:04:21 +0200

    new importing with lib-export #13

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -1,1 +1,7 @@
+/*  Only used once
+    Can be removed when using the main file  
+*/
+const importLib = require('/lib/import');
+importLib.init();
+
 //log.info('Hello from src/main/resources/main.js');
